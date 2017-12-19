30de006279f24be83cb3c73b3630d75e21d0d482
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-19 15:21:41 -0800

    Code Cleanup.  Created new Group Folders for set up.

--- breakpoint/Controller/FirstViewController.swift
+++ breakpoint/Controller/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  breakpoint
 //
 //  Created by Donald Belliveau on 2017-12-19.
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class FirstViewController: UIViewController {
+    
+    /*
+     Functions
+     */
+    
+    
+    /* View Did Load Function. */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+// END View Did Load Function.
+    
+    
+    /* Did Receive Memory Warning Function. */
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+// END Did Receive Memory Warning Function.
+    
+    
+} // End Class.
 
-
-}
-
+/*
+ 
+ VC1:
+ 
+ 
+ */
--- breakpoint/Controller/SecondViewController.swift
+++ breakpoint/Controller/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  breakpoint
 //
 //  Created by Donald Belliveau on 2017-12-19.
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class SecondViewController: UIViewController {
+    
+    /*
+     Functions.
+     */
+    
+    
+    /* View Did Load Function. */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+// END View Did Load Function.
+    
+    
+    /* Did Receive Memory Warning Function. */
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+// END Did Receive Memory Warning Function.
+    
+    
+} // END Class.
 
-
-}
-
+/*
+ 
+ VC2:
+ 
+ 
+ */
