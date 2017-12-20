a000144bf2753f02e4e5389c12f70a9585b54c99
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-20 10:31:57 -0800

    Added new Cocoa Touch file to View Folder.  Created class ShadowView.  Created Setup View Function.  Moved code from Set Up View to newly created Awake From Nib Function as only will ever be used the once.

---
+++ breakpoint/View/ShadowView.swift
@@ -0,0 +1,25 @@
+//
+//  ShadowView.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-20.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    // Functions.
+    
+    
+    /* Awake From Nib Function. */
+    override func awakeFromNib() {
+        self.layer.shadowOpacity = 0.75
+        self.layer.shadowRadius = 5 // Makes it look like the view is floating off the screen.
+        self.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        super.awakeFromNib()
+    } // END Awake From Nib Function.
+    
+    
+} // END Class.
