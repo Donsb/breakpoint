8498b6e34ff2f8b9fac16301d2bd3862f00d000c
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-20 09:58:52 -0800

    Added new Coca Touch file to Controller Folder.  Created class AuthVC.  General code cleanup.  Set Login storyboard screen to AuthVC class.  Gave it a Storyboard ID.  Added new Cocoa Touch file to Controller Folder.  Created class LoginVC.  General code cleanup.  Set login by email storyboard to class LoginVC.  Gave it a Storyboard ID.

---
+++ breakpoint/Controller/AuthVC.swift
@@ -0,0 +1,31 @@
+//
+//  AuthVC.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-20.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+    
+    // Functions.
+    
+    
+    /* View Did Load Function. */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    } // END View Did Load Function.
+    
+    
+    /* Did Receive Memory Warning Function. */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } // END Did Receive Memory Warning Function.
+    
+    
+} // END Class.
