16c6a1a1d9bc374f69c952355ac4311d7ee31010
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-19 16:54:52 -0800

    Set the instance variables.

--- breakpoint/Services/DataService.swift
+++ breakpoint/Services/DataService.swift
@@ -18,7 +18,26 @@ class DataService {
     
     // Instance Variables
     
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users") // Creates a Folder to hold all the users in database.
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
     
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_GROUPS: DatabaseReference {
+        return _REF_GROUPS
+    }
+    
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
     
 } // END Class.
 
