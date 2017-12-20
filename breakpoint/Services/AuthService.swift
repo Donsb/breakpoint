e0c4595d439b285a58b7b689dfabf96621e99c5d
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-20 11:07:40 -0800

    Imported Firebase.  Finished both functions in AuthService to register and login.

--- breakpoint/Services/AuthService.swift
+++ breakpoint/Services/AuthService.swift
@@ -17,13 +17,31 @@ class AuthService {
     // Functions.
     
     /* Register User Function. */
-    func registerUser() {
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?)-> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData) // .uid comes from firebase.
+            userCreationComplete(true, nil)
+        }
         
     } // END Register User Function.
     
     
     /* Login User Function. */
-    func loginUser() {
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?)-> ()) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
         
     } // END Login User Function.
     
