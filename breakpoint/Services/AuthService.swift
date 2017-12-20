52145b46b8583ea76d8ae863c29070ee5c9942f8
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-20 10:53:32 -0800

    Added new Swift file to Services.  Created class AuthService as a Singleton.

---
+++ breakpoint/Services/AuthService.swift
@@ -0,0 +1,21 @@
+//
+//  AuthService.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-20.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    
+    // Singleton
+    static let instance = AuthService()
+    
+    // Functions.
+    
+    
+    
+}
