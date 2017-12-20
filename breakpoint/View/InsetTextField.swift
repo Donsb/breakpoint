2c4db42abfef1eb03126997d3a09a1e42e1753ee
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-20 10:15:19 -0800

    Finished Functions: textRect; editingRect and placeholderRect

--- breakpoint/View/InsetTextField.swift
+++ breakpoint/View/InsetTextField.swift
@@ -12,31 +12,31 @@ class InsetTextField: UITextField {
     
     // Instance Variables
     
-    private var textRectOffset: CGFloat = 20 // Off set amount off the left of the textfield box.
     private var padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
     
     // Functions
     
     
     /* Text Rect Function. */
-    
-    override func textRect(forBounds bounds: CGRect) -> CGRect {
         //-> For where the Text is held.
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
     } // END Text Rect Function.
     
     
     /* Editing Rect Function. */
-    
-    override func editingRect(forBounds bounds: CGRect) -> CGRect {
         //-> Where your editing.
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
     } // END Editing Rect Function.
     
     
     /* Placeholder Rect Function. */
-    
-    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
         //-> Where your placeholder is.
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
     } // END Placeholder Rect Function.
     
     
 } // END Class.
+
