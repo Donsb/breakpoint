2848bd149613c02ee958f2cb5dcbfefa89669d7e
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-21 09:54:35 -0800

    Added UI Elements to TableView Cell. Added new Cocoa Touch File to View Folder.  General Code cleanup.

---
+++ breakpoint/View/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-21.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
