a827c847fc9e5a70fc90846511f77ec7543b1774
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-21 10:37:35 -0800

    Created Get Email Function in DataService class.

--- breakpoint/Services/DataService.swift
+++ breakpoint/Services/DataService.swift
@@ -93,6 +93,23 @@ class DataService {
     } // END Get All Feed Messages Function.
     
     
+    /* Get Email Function. */
+    func getEmail(forSearchQuery query: String, handler: @escaping (_ emailArray: [String])-> ()) {
+        var emailArray = [String]()
+        REF_USERS.observe(.value) { (userSnapshot) in
+            guard let userSnapshot = userSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            for user in userSnapshot {
+                let email = user.childSnapshot(forPath: "email").value as! String
+                
+                if email.contains(query) == true && email != Auth.auth().currentUser?.email {
+                    emailArray.append(email)
+                }
+            }
+            handler(emailArray)
+        }
+    } // END Get Email Function.
+    
+    
 } // END Class.
 
 
