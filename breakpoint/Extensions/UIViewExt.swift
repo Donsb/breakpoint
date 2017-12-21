8c8679c0932d13b5bd1d99a0661af51a7ed68244
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-20 16:14:19 -0800

    Added new Swift file in Extensions folder to have the Send Button on Message's screen to go up with the keyboard.  Added 2 Functions: Keyboard Will Change Function and Bind to Keyboard Function.

---
+++ breakpoint/Extensions/UIViewExt.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewExt.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-20.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    /* Bind to Keyboard Function. */
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    } // END Bind to Keyboard Function.
+    
+    
+    /* Keyboard Will Change Function. */
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let beginningFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endFrame.origin.y - beginningFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+        }, completion: nil)
+    } // END Keyboard Will Change Function.
+    
+} // END Extensions.
