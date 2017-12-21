9d42eb524fb14c93b3bf46128294940f54afa49e
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-21 06:15:20 -0800

    Added UI Elements to Table View Cell in Feed Storyboard.  Added new Cocoa Touch file to View Folder.  Created class FeedCell.

---
+++ breakpoint/View/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-21.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+    
+    
+    
+} // END Class.
