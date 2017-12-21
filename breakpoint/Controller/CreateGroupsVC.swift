47b5feb18495f07be282637f8546ad49c7bbfe45
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-21 08:54:50 -0800

    Added VC to storyboard for Create a New Group chat.  Added new Cocoa Touch file to Controller folder.  Created class CreateGroupsVC.

---
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,51 @@
+//
+//  CreateGroupsVC.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-21.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+    
+    // Functions.
+    
+    
+    /* View Did Load Function. */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    } // END iew Did Load Function.
+    
+    
+    /* Did Receive Memory Warning Function. */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } // END Did Receive Memory Warning Function.
+    
+    
+} // END Class.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
