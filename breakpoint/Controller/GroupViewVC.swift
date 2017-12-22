282c7736f4143bb071d61b6b29c8233a8848f591
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-21 17:07:39 -0800

    Added new Cocoa Touch file to Controller folder and created class GroupViewVC

---
+++ breakpoint/Controller/GroupViewVC.swift
@@ -0,0 +1,30 @@
+//
+//  GroupViewVC.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-21.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewVC: UIViewController {
+    
+    // Functions
+    
+    /* View Did Load Function. */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    } // END View Did Load Function.
+    
+    
+    /* Did Receive Memory Warning Function. */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } // END Did Receive Memory Warning Function.
+    
+    
+} // END Class.
