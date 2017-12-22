020a16cde4342417102d0a908e51d20af06dab3b
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-22 10:14:43 -0800

    Finished the uplaodPosts Function.  Inside DataService, created Get All Messages For Function.

--- breakpoint/Services/DataService.swift
+++ breakpoint/Services/DataService.swift
@@ -68,7 +68,7 @@ class DataService {
     func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupKey: String?, sendComplete: @escaping (_ status: Bool)-> ()) {
         
         if groupKey != nil {
-            // Send to group ref
+            REF_GROUPS.child(groupKey!).child("messages").childByAutoId().updateChildValues(["content": message, "senderId": uid])
         } else {
             REF_FEED.childByAutoId().updateChildValues(["content": message, "senderId": uid])
             sendComplete(true)
@@ -91,6 +91,23 @@ class DataService {
             handler(messageArray)
         }
     } // END Get All Feed Messages Function.
+    
+    
+    /* Get All Messages For Function. */
+    func getAllMessagesFor(desiredGroup: Group, handler: @escaping (_ messagesArray: [Message])-> ()) {
+        
+        var groupMessageArray = [Message]()
+        REF_GROUPS.child(desiredGroup.key).child("messages").observeSingleEvent(of: .value) { (groupMessageSnapshot) in
+            guard let groupMessageSnapshot = groupMessageSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            for groupMessage in groupMessageSnapshot {
+                let content = groupMessage.childSnapshot(forPath: "content").value as! String
+                let senderId = groupMessage.childSnapshot(forPath: "senderId").value as! String
+                let groupMessage = Message(content: content, senderId: senderId)
+                groupMessageArray.append(groupMessage)
+            }
+            handler(groupMessageArray)
+        }
+    } // END Get All Messages For Function.
     
     
     /* Get Email Function. */
