aff7af453ed66f5fc8f8aa5363991c754f53dbfc
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-21 17:03:22 -0800

    Added new Storyboard for Group Chat.  Added new Cocoa Touch file to View and created class GroupFeedCell.

---
+++ breakpoint/View/GroupFeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  GroupFeedCell.swift
+//  breakpoint
+//
+//  Created by Donald Belliveau on 2017-12-21.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedCell: UITableViewCell {
+    
+    
+    
+} // END Class.
